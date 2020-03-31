fd3ee696d902a2e02793692698d9b117d65f1a99
vchambers6 <35242014+vchambers6@users.noreply.github.com>
2020-03-31 10:26:36 -0500

    Compiles, n found
    Found n, varies between 32 and 64

--- strassen.java
+++ strassen.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class strassen {
 
-	public static int[][] naive(int [][] matrix1, int [][] matrix2){
+	public int[][] naive(int [][] matrix1, int [][] matrix2){
 
 		int n = matrix1.length;
 		int result[][]=new int[n][n];
@@ -38,77 +38,91 @@ public class strassen {
 	ArrayList<Object> listInner = new ArrayList();
 	linstInner.add()
 	*/
-	public static int[][] strassen(int[][] mat1, int[][] mat2) {
+	public int[][] strassen(int[][] mat1, int[][] mat2, int k) {
 
 		int n = mat1.length;
 		int[][] product = new int[n][n];
-
-		if (mat1.length == 1) {
-			product[0][0] = mat1[0][0] * mat2[0][0];
+		
+		if (k == n) {
+			naive(mat1, mat2); 
 		}
-
 		else {
-			int [][] a = new int[n/2][n/2];
-			int [][] b = new int[n/2][n/2];
-			int [][] c = new int[n/2][n/2];
-			int [][] d = new int[n/2][n/2];
-			int [][] e = new int[n/2][n/2];
-			int [][] f = new int[n/2][n/2];
-			int [][] g = new int[n/2][n/2];
-			int [][] h = new int[n/2][n/2];
-			// also consider just having empty lists and then adding to them
-			// in the funciton rather than indexing through them
 
-			// Splits mat1 and mat2 into 4 parts each
-			for (int i = 0; i < (n/2); i++) {
-				for (int j = 0; j < (n/2); j++){
-					a[i][j] = mat1[i][j];
-					b[i][j] = mat1[i][j + n/2];
-					c[i][j] = mat1[i + n/2][j];
-					d[i][j] = mat1[i + n/2][j + n/2];
+			if (mat1.length == 1) {
+				product[0][0] = mat1[0][0] * mat2[0][0];
+			}
 
-					e[i][j] = mat2[i][j];
-					f[i][j] = mat2[i][j + n/2];
-					g[i][j] = mat2[i + n/2][j];
-					h[i][j] = mat2[i + n/2][j + n/2];
+			else {
+				int [][] a = new int[n/2][n/2];
+				int [][] b = new int[n/2][n/2];
+				int [][] c = new int[n/2][n/2];
+				int [][] d = new int[n/2][n/2];
+				int [][] e = new int[n/2][n/2];
+				int [][] f = new int[n/2][n/2];
+				int [][] g = new int[n/2][n/2];
+				int [][] h = new int[n/2][n/2];
+				// also consider just having empty lists and then adding to them
+				// in the funciton rather than indexing through them
 
+				// Splits mat1 and mat2 into 4 parts each
+				for (int i = 0; i < (n/2); i++) {
+					for (int j = 0; j < (n/2); j++){
+						a[i][j] = mat1[i][j];
+						b[i][j] = mat1[i][j + n/2];
+						c[i][j] = mat1[i + n/2][j];
+						d[i][j] = mat1[i + n/2][j + n/2];
+
+						e[i][j] = mat2[i][j];
+						f[i][j] = mat2[i][j + n/2];
+						g[i][j] = mat2[i + n/2][j];
+						h[i][j] = mat2[i + n/2][j + n/2];
+
+
+					}
+					/*a[i] = mat1[i].subList(0, n/2);
+					b[i] = mat1[i].subList(n/2, n);
+					e[i] = mat2[i].subList(0, n/2);
+					f[i] = mat2[i].subList(n/2, n);
+					c[i] = mat1[n/2 + i].subList(0, n/2);
+					d[i] = mat1[n/2 + i].subList(n/2, n);
+					g[i] = mat2[n/2 + i].subList(0, n/2);
+					h[i] = mat2[n/2 + i].subList(n/2, n); */
 
 				}
-				/*a[i] = mat1[i].subList(0, n/2);
-				b[i] = mat1[i].subList(n/2, n);
-				e[i] = mat2[i].subList(0, n/2);
-				f[i] = mat2[i].subList(n/2, n);
-				c[i] = mat1[n/2 + i].subList(0, n/2);
-				d[i] = mat1[n/2 + i].subList(n/2, n);
-				g[i] = mat2[n/2 + i].subList(0, n/2);
-				h[i] = mat2[n/2 + i].subList(n/2, n); */
+				
+				int [][] p1 = strassen(a, subtract(f, h), k);
+				int [][] p2 = strassen(add(a, b), h, k);
+				int [][] p3 = strassen(add(c, d), e, k);
+				int [][] p4 = strassen(d, subtract(g, e), k);
+				int [][] p5 = strassen(add(a, d), add(e, h), k);
+				int [][] p6 = strassen(subtract(b, d), add(g, h), k);
+				int [][] p7 = strassen(subtract(a, c), add(e, f), k);
+				
 
-			}
-			int [][] p1 = strassen(a, subtract(f, h));
-			int [][] p2 = strassen(add(a, b), h);
-			int [][] p3 = strassen(add(c, d), e);
-			int [][] p4 = strassen(d, subtract(g, e));
-			int [][] p5 = strassen(add(a, d), add(e, h));
-			int [][] p6 = strassen(subtract(b, d), add(g, h));
-			int [][] p7 = strassen(subtract(a, c), add(e, f));
+				// Resulting quadrants, first being the upper left and second the upper
+				// right
+				/*int [][] first = add(subtract(add(strassen(add(a, d), add(e, h)), strassen(d, subtract(g, e))), strassen(add(a, b), h)), strassen(subtract(b, d), add(g, h)));
+				int [][] second =  add(strassen(a, subtract(f, h)), strassen(add(a, b), h));
+				int [][] third = add(strassen(add(c, d), e), strassen(d, subtract(g, e), ));
+				int [][] fourth = subtract(subtract(add(strassen(add(a, d), add(e, h)), strassen(a, subtract(f, h))), strassen(add(c, d), e)), strassen(subtract(a, c), add(e, f)));
+				*/
+				int [][] first = add(subtract(add(p5, p4), p2), p6);
+				int [][] second =  add(p1, p2);
+				int [][] third = add(p3, p4);
+				int [][] fourth = subtract(subtract(add(p5, p1), p3), p7);
 
-			// Resulting quadrants, first being the upper left and second the upper
-			// right
-			int [][] first = add(subtract(add(p5, p4), p2), p6);
-			int [][] second =  add(p1, p2);
-			int [][] third = add(p3, p4);
-			int [][] fourth = subtract(subtract(add(p5, p1), p3), p7);
-
-			// Recombines split comoponents
-			for (int i = 0; i < (n/2); i++) {
-				for(int j = 0; j < n/2; j++) {
-					product[i][j] = first[i][j];
-					product[i][j + n/2] = second[i][j];
-					product[i + n/2][j] = third[i][j];
-					product[i + n/2][j + n/2] = fourth[i][j];
+				// Recombines split comoponents
+				for (int i = 0; i < (n/2); i++) {
+					for(int j = 0; j < n/2; j++) {
+						product[i][j] = first[i][j];
+						product[i][j + n/2] = second[i][j];
+						product[i + n/2][j] = third[i][j];
+						product[i + n/2][j + n/2] = fourth[i][j];
+					}
 				}
 			}
 		}
+		
 		/*System.out.print("Strassen result: \n");
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < n; j++){
@@ -122,7 +136,7 @@ public class strassen {
 	}
 
 	// Matrix Addition Function
-    public static int[][] add(int[][] mat1, int[][] mat2){
+    public int[][] add(int[][] mat1, int[][] mat2){
     	int dim = mat1.length;
     	int[][] sum = new int[dim][dim];
 
@@ -134,7 +148,7 @@ public class strassen {
     	return sum;
     }
 
-    public static int[][] subtract(int[][] mat1, int[][] mat2){
+    public int[][] subtract(int[][] mat1, int[][] mat2){
     	int dim = mat1.length;
     	int[][] diff = new int[dim][dim];
 
@@ -146,7 +160,7 @@ public class strassen {
     	return diff;
     }
 
-    public static int test1(int dim) {
+    public int test1(int dim, int k) {
     	int [][] mat1 = new int[dim][dim]; 
     	int [][] mat2 = new int[dim][dim]; 
 
@@ -187,7 +201,7 @@ public class strassen {
 		*/
 		System.out.println("dimension: " + dim);
 		long start1 = System.nanoTime(); 
-		int [][] product = strassen(mat1, mat2);
+		int [][] product = strassen(mat1, mat2, k);
 		long end1 = System.nanoTime(); 
 		long elapsed1 = end1 - start1; 
 		System.out.println("Strassens takes " + elapsed1 + " nanoseconds");
@@ -199,10 +213,10 @@ public class strassen {
 		System.out.println("Naive takes " + elapsed2 + " nanoseconds"); 
 
 		if ((elapsed1 - elapsed2) <0){
-			System.out.println("n: " + dim);
+			System.out.println("n: " + k);
 		}
 		else {
-			test1(dim*2); 
+			test1(dim, k*2); 
 		}
 		return dim; 
     }
@@ -242,8 +256,10 @@ public class strassen {
 			}
 			System.out.println();
 		} */
-		int dim = 2; 
-		int result = test1(dim); 
+		strassen s = new strassen();
+		int dim = 256;
+		int k = 2; 
+		int result = s.test1(dim, k); 
 		
 
 	}
