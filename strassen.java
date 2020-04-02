ba11506035fb3223a7210414b9c59740ee7e6b41
vchambers6 <35242014+vchambers6@users.noreply.github.com>
2020-04-01 23:54:18 -0500

    Works for all matrix powers of 2
    Works and compiles in the gradescope. Does not work for non power of 2 matrices

--- strassen.java
+++ strassen.java
@@ -7,41 +7,33 @@ import java.util.Random;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
+import java.util.Random;
 
 
-public class strassen {
+public class Strassen {
 
 	public int[][] naive(int [][] matrix1, int [][] matrix2){
 
 		int n = matrix1.length;
 		int result[][]=new int[n][n];
 
-        //multiplying matrices
+        // Multiplying matrices
         for(int i = 0; i < n; i++){
 
             for(int j = 0; j < n; j++){
 
-								// find entries per row
+				// Find entries per row
                 result[i][j]=0;
 
                 for(int k = 0; k <n; k++)
                 {
                     result[i][j] += (matrix1[i][k] * matrix2[k][j]);
                 }
-
-                // printing current matrix element
             }
-						// print newline for next row
         }
         return result;
 
 	}
-	/*
-	consider this implementation
-	ArrayList<List<Object>> mat1 = new ArrayList();
-	ArrayList<Object> listInner = new ArrayList();
-	linstInner.add()
-	*/
 	public int[][] strassen(int[][] mat1, int[][] mat2, int k) {
 
 		int n = mat1.length;
@@ -80,11 +72,9 @@ public class strassen {
 						f[i][j] = mat2[i][j + n/2];
 						g[i][j] = mat2[i + n/2][j];
 						h[i][j] = mat2[i + n/2][j + n/2];
-
 					}
-
 				}
-				
+
 				// 7 multiplications required for strassens 
 				int [][] p1 = strassen(a, subtract(f, h), k);
 				int [][] p2 = strassen(add(a, b), h, k);
@@ -112,6 +102,7 @@ public class strassen {
 			}
 		}
 		
+		// If needed: print out matrix
 		/*System.out.print("Strassen result: \n");
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < n; j++){
@@ -157,10 +148,10 @@ public class strassen {
     	if (k > dim) {
     		// Default "Breaking Point" is 2 
     		if (breakpt == 2) {
-    			System.out.println("n0 not found. ");
+    			//System.out.println("n0 not found. ");
     		}
     		else {
-    			System.out.println("Optimal Breakpoint: " + breakpt);
+    			//System.out.println("Optimal Breakpoint: " + breakpt);
     		}
     		
     		return breakpt;  
@@ -183,25 +174,20 @@ public class strassen {
 				System.out.println();
 			}
 			*/
-			System.out.println("n: " + k);
+
 			// Start timer 
 			long start1 = System.nanoTime(); 
 			// Run strassen
 			int [][] product = strassen(mat1, mat2, k);
+			// End timer & calculate elapsed time of strassen
 			long end1 = System.nanoTime(); 
 			long elapsed1 = end1 - start1; 
-			System.out.println("Strassens takes " + elapsed1 + " nanoseconds");
+			//System.out.println("Strassens takes " + elapsed1 + " nanoseconds");
 
-			/*long start2 = System.nanoTime(); 
-			int[][] tester = naive(mat1, mat2);
-			long end2 = System.nanoTime(); 
-			long elapsed2 = end2 - start2; 
-			System.out.println("Naive takes " + elapsed2 + " nanoseconds"); 
-			*/
-			
+			// Checks if new crossover point is more optimal 		
 			if (elapsed1 < time) {
-				System.out.println("n0: " + k);
-				System.out.println("time spent: " + elapsed1); 
+				//System.out.println("n0: " + k);
+				//System.out.println("time spent: " + elapsed1); 
 				test1(mat1, mat2, dim, k*2, elapsed1, k);
 			}
 			else
@@ -211,57 +197,58 @@ public class strassen {
 	    }
     	
     }
+
     public int[][] generateRandomMat(int n, double p){
 
-		// create empty matrix of size n
+		// Create empty matrix of size n
 		int dim = n;
 		int [][] mat = new int[dim][dim];
-
-		// populate matrix with 0s and 1's based off given probability p
+		int edges = 0; 
+		// Populate matrix with 0s and 1's based off given probability p
     	for (int i = 0; i < dim; i++) {
 	    	for (int j = 0; j < dim; j++){
-					// generate random number for probability
-					double rand1 = Math.random();
-
-					// to prevent edges that come from same vertex and point to same vertex
-					if (i == j){
-
-					  mat[i][j] = 0;
-
-					}
-					// if p is less than or equal to rand
-    			else if (rand1 <= p) {
-
-    				mat[i][j] = 1;
-
-    			}
-    			else {
+				// Generate random number for probability
+				double rand1 = Math.random();
+				
+				// To prevent edges that come from same 
+				// vertex and point to same vertex
+				if (i == j){
+				  mat[i][j] = 0;
+				}
+				else if (mat[i][j] == 0) {
+					if (rand1 <= p) {
+						edges++; 
+	    				mat[i][j] = 1;
+	    				//mat[j][i] = 1; 
+	    				}
+    				else {
     				mat[i][j] = 0;
-    			}
-
+    				}
+				}
     		}
     	}
-
-	return mat;
+		return mat;
 	}
 
-    public int numTriangles(int p){
+	// Calcultaes the number of triangles in 1024 vertex graph
+    public int numTriangles(double p){
 
 		// generate random matrix
-		int [][] randomMat = generateRandomMat(1024, p);
-
-
+		int d = 1024;
+		int [][] randomMat = generateRandomMat(d, p);
+		
 		// variable to store sum of diagonal entries
 		int addDiagonal = 0;
 
 		// find cube of random matrix
-		int [][] randomMatSq = strassen(randomMat, randomMat, 1024);
-		int [][] randomMatCube = strassen (randomMatSq, randomMat, 1024);
+		int [][] randomMatSq = strassen(randomMat, randomMat, 32);
+		int [][] randomMatCube = strassen (randomMatSq, randomMat, 32);
 
 		// find total of diagonals of the cubed matrix
-		for(int i = 0; i < 1024; i++){
+		for(int i = 0; i < d; i++){ 
 
-			addDiagonal =+ randomMatCube[i][i];
+			addDiagonal += randomMatCube[i][i];
+			//System.out.println("diagonal val: " + addDiagonal);
 		}
 
 		int numTriangles = addDiagonal/6;
@@ -270,6 +257,27 @@ public class strassen {
 	}
 
 	public static void main(String[] args) throws FileNotFoundException{
+		
+		// Declare new object
+		Strassen s = new Strassen();
+
+		// Calculates triangles in diff graphs for different probabilities
+		/*int p1 = s.numTriangles(.01);
+		int p2 = s.numTriangles(.02);
+		int p3 = s.numTriangles(.03);
+		int p4 = s.numTriangles(.04);
+		int p5 = s.numTriangles(.05);
+
+		System.out.println("num triangles when p = .01: " + p1);
+		System.out.println("num triangles when p = .02: " + p2);
+		System.out.println("num triangles when p = .03: " + p3);
+		System.out.println("num triangles when p = .04: " + p4);
+		System.out.println("num triangles when p = .05: " + p5);
+		*/
+
+
+		
+
 		/*int dim = 1024;
 		int [][] mat1 = new int[dim][dim]; 
     	int [][] mat2 = new int[dim][dim]; 
@@ -295,53 +303,16 @@ public class strassen {
     		}
 		*/
 
-		/*int [] l1 = {2, 1, 4, 7};
-		int [] l2 = {4, 6, 7, 100};
-		int [] l3 = {7529, 42, 1, 7};
-		int [] l4 = {4, 7, 2, 9};
-
-		int [] l5 = {4, 9 , 1, 1};
-		int [] l6 = {10, 13, 5, 6};
-		int [] l7 = {11, 300, 65, 0}; 
-		int [] l8 = {1, 1, 69, 1}; 
-
-		int [][] test1 = {l1, l2, l3, l4};
-		int [][] test2 = {l5, l6, l7, l8};
-
-		int[][] product;
-		long start1 = System.nanoTime(); 
-		product = strassen(test1, test2);
-		long end1 = System.nanoTime(); 
-		long elapsed1 = end1 - start1; 
-		System.out.println("Strassens takes " + elapsed1 + " nanoseconds");
-
-		long start2 = System.nanoTime(); 
-		int[][] tester = naive(test1, test2);
-		long end2 = System.nanoTime(); 
-		long elapsed2 = end2 - start2; 
-		System.out.println("Naive takes " + elapsed2 + " nanoseconds"); 
-
-		System.out.print("Naive result: \n");
-		for(int i = 0; i < 4; i++){
-			for(int j = 0; j < 4; j++){
-				System.out.print(tester[i][j] + " ");
-			}
-			System.out.println();
-		} */
+		
+	// Stores dimension and input file from command line
 	int dim = Integer.parseInt(args[1]); 
-	Scanner scn = new Scanner(new File(args[2]));
-
-    int size = scn.nextInt(); 
-    int [] array = new int[size]; 
     Scanner input = new Scanner(new File(args[2]));
     
-    /*for (int i = 0; i < size; i ++ ) {
-    	int j = hi.nextInt(); 
-    	System.out.println("this is a test: " + j);
-    }*/
+    // Declare matrix to store values from input file 
     int [][] mat1 = new int[dim][dim]; 
     int [][] mat2 = new int[dim][dim];
     int d2 = dim*dim;  
+    // Before the file ends, populate matrices 
     while(input.hasNextInt()) {
     	int i = 0; 
 		for (int j = 0; j < 2*dim; j++) {
@@ -358,7 +329,7 @@ public class strassen {
 		}
     }
 
-    System.out.println("Matrix 1: "); 
+    /*System.out.println("Matrix 1: "); 
 	    	for(int i = 0; i < dim; i++){
 				for(int j = 0; j < dim; j++){
 					System.out.print(mat1[i][j] + " ");
@@ -372,40 +343,42 @@ public class strassen {
 				}
 				System.out.println();
 			}
-    
-
-    /*while(input.hasNextInt())
-	{
-   		int j = input.nextInt();
-   		System.out.println("this is a test: " + j);
-    }
-    */
-		strassen s = new strassen();
 		
+		*/
+
 		int k = 2; 
-		long start2 = System.nanoTime(); 
+
+		//Start timer to generate naive time, optional 
+		/*long start2 = System.nanoTime(); 
 			int[][] tester = s.naive(mat1, mat2);
 			long end2 = System.nanoTime(); 
 		long elapsed2 = end2 - start2; 
-		System.out.println("Naive takes " + elapsed2 + " nanoseconds"); 
-		int testing = s.test1(mat1, mat2, dim, k, elapsed2, k);
-		System.out.println("Resulting matrix: ");
+		System.out.println("time: " + elapsed2);*/
 		
-		int [][] result = s.strassen(mat1, mat2, testing);
+		//System.out.println("Naive takes " + elapsed2 + " nanoseconds"); 
 
+		// Generates ideal crossover value
+		//int testing = s.test1(mat1, mat2, dim, k, 2147483647, k);
+		//System.out.println("Resulting matrix: ");
+		
+		// Runs strassens, random breakpoint 32 tested. 
+		int [][] result = s.strassen(mat1, mat2, 32);
 
+		// Prints out diagonal entries 
 		for (int i = 0; i < dim; i++) {
 			for (int j = 0; j < dim; j++) {
 				if (i == j) {
-					System.out.print(result[i][j] + " "); 
+					System.out.print(result[i][j]); 
 				}
-				System.out.print(" "); 
+				//System.out.print(" "); 
 			}
 			System.out.println();
 
 		}
+		System.out.println("");
 		
 
 	}
+	
 }
 
